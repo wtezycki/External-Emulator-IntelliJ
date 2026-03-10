d94720c5e854671e9ce25db139e06ef3ce9b2e62
Wiktor Tężycki <wtezycki1@gmail.com>
2026-03-10 14:40:05 +0100

    Cursor position management and validation on bounds

--- src/core/TerminalBuffer.java
+++ src/core/TerminalBuffer.java
@@ -26,8 +26,8 @@ public class TerminalBuffer {
     private final Deque<TerminalLine> scrollback;
 
     // Cursor:
-    private int cursor_x;
-    private int cursor_y;
+    private int cursorX;
+    private int cursorY;
 
     // Attributes:
     private CellAttribute currAttribute;
@@ -35,19 +35,38 @@ public class TerminalBuffer {
     public TerminalBuffer(int width, int height, int maxScrollback) {
         this.width = width;
         this.height = height;
-        screen = new ArrayList<TerminalLine>(height);
+        screen = new ArrayList<>(height);
 
         this.maxScrollback = maxScrollback;
-        this.scrollback = new ArrayDeque<TerminalLine>();
+        this.scrollback = new ArrayDeque<>();
 
-        this.cursor_x = 0;
-        this.cursor_y = 0;
+        this.cursorX = 0;
+        this.cursorY = 0;
 
         this.currAttribute = new CellAttribute();
 
         for (int i = 0; i < height; i++) { screen.add(new TerminalLine(width)); }
     }
 
+    // Cursor methods:
+    public void setCursorPosition(int x, int y) {
+        // Check boundaries
+        // Method 1: Block invalid position
+//        if ((x < 0 || x >= width) ||
+//                (y < 0 || y >= height) ) {
+//            return;
+//        }
+//        this.cursorX = x;
+//        this.cursorY = y;
 
+        // Method 2 (better): Allow user to go to the most valid cell
+        this.cursorX = Math.max(0, Math.min(x, width - 1));
+        this.cursorY = Math.max(0, Math.min(y, height - 1));
+    }
+
+    public void moveCursor(int dx, int dy) {
+        setCursorPosition(cursorX + dx,
+                        cursorY + dy);
+    }
 
 }
--- src/models/TerminalLine.java
+++ src/models/TerminalLine.java
@@ -1,6 +1,7 @@
 package models;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 // Manages one row
@@ -24,20 +25,21 @@ public class TerminalLine {
     }
 
     public void setCell(int x, Cell cell) {
-        if (x >= 0 && x <= width) {
+        if (x >= 0 && x < width) {
             row.set(x, cell); // set cell on index x
         }
     }
 
     public Cell getCell(int x) {
-        if (x >= 0 && x <= width) {
+        if (x >= 0 && x < width) {
             return row.get(x);
         }
         // for safety
         return Cell.createEmpty();
     }
 
-    public List<Cell> getRow() { return row; }
+    // Return unmodifiable list so it is blocked to change state of the row from the outside.
+    public List<Cell> getRow() { return Collections.unmodifiableList(row); }
     public int getWidth() { return width; }
 
 }
